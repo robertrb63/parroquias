7c67c6b6816302d3f81ae5ae1c663453a568fddc
robertrb63 <81638977+robertrb63@users.noreply.github.com>
2025-04-07 21:09:04 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,34 @@
+import requests
+import streamlit as st
+
+# Encabezado
+st.title("Obispado Barbastro Monzón")
+
+# Buscar registros
+st.header("Buscar registros")
+query = st.text_input("Nombre a buscar:")
+if st.button("Buscar"):
+    response = requests.get('http://127.0.0.1:5000/buscar', params={'query': query})
+    resultados = response.json()
+    st.write(resultados)
+
+# Crear registros
+st.header("Crear registros")
+password = st.text_input("Contrasena:", type="password")
+nombre = st.text_input("Nombre:")
+telefono = st.text_input("Teléfono:")
+email = st.text_input("Email:")
+# Agrega aquí más campos según tu base de datos
+if st.button("Crear"):
+    nuevo_registro = {
+        "nombre": nombre,
+        "telefono": telefono,
+        "email": email,
+        # Agrega más campos
+    }
+    response = requests.post('http://127.0.0.1:5000/crear', json={"password": password, "registro": nuevo_registro})
+    st.write(response.json())
+
+# Actualizar y borrar registros
+st.header("Actualizar y borrar registros")
+# Implementa estas páginas según tus necesidades.
